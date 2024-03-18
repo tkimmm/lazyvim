ea9dd97c42b7d1f67a666b034dbc06081c0b9614
teekm@mbp2023 <tkimm@me.com>
2024-03-18 23:09:15 +0100

    update color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,58 +1,56 @@
 return {
-
-  -- {
-  --   "catppuccin/nvim",
-  --   name = "catppuccin",
-  --   priority = 2000,
-  --   config = function()
-  --     -- vim.cmd.colorscheme "catppuccin"
-  --     require("catppuccin").setup({
-  --       -- flavour = "mocha", -- latte, frappe, macchiato, mocha
-  --       flavour = "frappe", -- latte, frappe, macchiato, mocha
-  --       transparent_background = true,
-  --       background = {
-  --         light = "latte",
-  --         dark = "mocha",
-  --       },
-  --       show_end_of_buffer = false,
-  --       term_colors = false,
-  --       dim_inactive = {
-  --         enabled = false,
-  --         shade = "dark",
-  --         percentage = 0.15,
-  --       },
-  --       no_italic = false, -- Force no italic
-  --       no_bold = false, -- Force no bold
-  --       no_underline = false, -- Force no underline
-  --       styles = {
-  --         comments = { "italic" },
-  --         conditionals = { "italic" },
-  --         loops = {},
-  --         functions = {},
-  --         keywords = {},
-  --         strings = {},
-  --         variables = {},
-  --         numbers = {},
-  --         booleans = {},
-  --         properties = {},
-  --         types = {},
-  --         operators = {},
-  --       },
-  --       color_overrides = {},
-  --       custom_highlights = {},
-  --       integrations = {
-  --         cmp = true,
-  --         gitsigns = true,
-  --         nvimtree = true,
-  --         telescope = true,
-  --         notify = false,
-  --         mini = false,
-  --       },
-  --     })
-  --     vim.cmd.colorscheme("catppuccin")
-  --   end,
-  -- },
-
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      -- vim.cmd.colorscheme "catppuccin"
+      require("catppuccin").setup({
+        -- flavour = "mocha", -- latte, frappe, macchiato, mocha
+        flavour = "frappe", -- latte, frappe, macchiato, mocha
+        transparent_background = true,
+        background = {
+          light = "latte",
+          dark = "mocha",
+        },
+        show_end_of_buffer = false,
+        term_colors = false,
+        dim_inactive = {
+          enabled = false,
+          shade = "dark",
+          percentage = 0.15,
+        },
+        no_italic = false, -- Force no italic
+        no_bold = false, -- Force no bold
+        no_underline = false, -- Force no underline
+        styles = {
+          comments = { "italic" },
+          conditionals = { "italic" },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+        },
+        color_overrides = {},
+        custom_highlights = {},
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          telescope = true,
+          notify = false,
+          mini = false,
+          neotree = false,
+        },
+      })
+    end,
+  },
   {
     "EdenEast/nightfox.nvim",
     priority = 1000,
