bc51251573adb46b47d77940191618b4f872525d
teekm@mbp2023 <tkimm@me.com>
2024-03-19 15:13:07 +0100

    cfg change

--- lua/plugins/markdownpreview.lua
+++ lua/plugins/markdownpreview.lua
@@ -1,8 +1,11 @@
 return {
-  "iamcco/markdown-preview.nvim",
-  run = "cd app && npm install",
-  setup = function()
-    vim.g.mkdp_filetypes = { "markdown" }
-  end,
-  ft = { "markdown" },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 }
