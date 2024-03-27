a9bd76139900982c3813c8ff286e27a8bf42111f
teekm@mbp2023 <tkimm@me.com>
2024-03-27 19:02:29 +0100

    revert to tab file tree

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -32,42 +32,42 @@ return {
         prefix = "[FILTER]: ",
         always_show_folders = false, -- Turn into false from true by default
       },
-      -- view = {
-      -- width = 35,
-      -- signcolumn = "yes",
-      -- },
       view = {
-        float = {
-          enable = true,
-          open_win_config = function()
-            local screen_w = vim.opt.columns:get()
-            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
-            local window_w = screen_w * WIDTH_RATIO
-            local window_h = screen_h * HEIGHT_RATIO
-            local window_w_int = math.floor(window_w)
-            local window_h_int = math.floor(window_h)
-            local center_x = (screen_w - window_w) / 2
-            local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
-            return {
-              border = "rounded",
-              relative = "editor",
-              row = center_y,
-              col = center_x,
-              width = window_w_int,
-              height = window_h_int,
-            }
-          end,
-        },
-        width = function()
-          return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
-        end,
+        width = 35,
+        signcolumn = "yes",
       },
+      -- view = {
+      --   float = {
+      --     enable = true,
+      --     open_win_config = function()
+      --       local screen_w = vim.opt.columns:get()
+      --       local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+      --       local window_w = screen_w * WIDTH_RATIO
+      --       local window_h = screen_h * HEIGHT_RATIO
+      --       local window_w_int = math.floor(window_w)
+      --       local window_h_int = math.floor(window_h)
+      --       local center_x = (screen_w - window_w) / 2
+      --       local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+      --       return {
+      --         border = "rounded",
+      --         relative = "editor",
+      --         row = center_y,
+      --         col = center_x,
+      --         width = window_w_int,
+      --         height = window_h_int,
+      --       }
+      --     end,
+      --   },
+      --   width = function()
+      --     return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+      --   end,
+      -- },
       renderer = {
         group_empty = true, -- default: true. Compact folders that only contain a single folder into one node in the file tree.
         highlight_git = false,
         full_name = true,
         highlight_opened_files = "icon", -- "none" (default), "icon", "name" or "all"
-        highlight_modified = "icon", -- "none", "name" or "all". Nice and subtle, override the open icon
+        highlight_modified = "icon",     -- "none", "name" or "all". Nice and subtle, override the open icon
         root_folder_label = ":~:s?$?/..?",
         indent_width = 2,
         indent_markers = {
@@ -98,10 +98,10 @@ return {
             default = "",
             symlink = "",
             bookmark = "",
-            modified = "", -- default: ● - Rather change background color
+            modified = "",        -- default: ● - Rather change background color
             folder = {
               arrow_closed = "-", -- default: "",
-              arrow_open = "+", -- default: "",
+              arrow_open = "+",   -- default: "",
               default = "",
               open = "",
               empty = "",
