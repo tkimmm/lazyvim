4d875cafabce915d770a799a1f20456a79d0ec96
teekm@mbp2023 <tkimm@me.com>
2024-04-01 23:53:11 +0200

    Disable dadbod

--- lua/plugins/db.lua
+++ lua/plugins/db.lua
@@ -1,9 +1,11 @@
+if true then return {} end
+
 return {
   {
     "kristijanhusak/vim-dadbod-ui",
     event = "VeryLazy",
     dependencies = {
-      { "tpope/vim-dadbod", event = "VeryLazy" },
+      { "tpope/vim-dadbod",                     event = "VeryLazy" },
       { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, event = "VeryLazy" },
     },
     keys = {
