0387f9260a75cc81ab3e155d3a7d209c6a58929f
teekm@mbp2023 <tkimm@me.com>
2024-04-25 15:58:15 +0200

    Open file on create by default

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -26,6 +26,12 @@ return {
         end
       end,
     })
+
+    local api = require("nvim-tree.api")
+    api.events.subscribe(api.events.Event.FileCreated, function(file)
+      vim.cmd("edit " .. file.fname)
+    end)
+
     require("nvim-tree").setup({
       live_filter = {
         prefix = "[FILTER]: ",
