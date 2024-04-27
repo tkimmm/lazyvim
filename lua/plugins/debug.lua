3c2454371c423029445e98a8d5933aba7f05b527
teekm@mbp2023 <tkimm@me.com>
2024-04-27 22:41:34 +0200

    fix: debug and luasnip

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,112 +1,3 @@
-if true then
-  return {
-    {
-      "mfussenegger/nvim-dap",
-      dependencies = {
-        -- fancy UI for the debugger
-        {
-          "rcarriga/nvim-dap-ui",
-          dependencies = { "nvim-neotest/nvim-nio" },
-          -- stylua: ignore
-          keys = {
-            { "<leader>du", function() require("dapui").toggle({}) end, desc = "Dap UI" },
-            { "<leader>de", function() require("dapui").eval() end,     desc = "Eval",  mode = { "n", "v" } },
-          },
-          opts = {},
-          config = function(_, opts)
-            -- setup dap config by VsCode launch.json file
-            -- require("dap.ext.vscode").load_launchjs()
-            local dap = require("dap")
-            local dapui = require("dapui")
-            dapui.setup(opts)
-            dap.listeners.after.event_initialized["dapui_config"] = function()
-              dapui.open({})
-            end
-            dap.listeners.before.event_terminated["dapui_config"] = function()
-              dapui.close({})
-            end
-            dap.listeners.before.event_exited["dapui_config"] = function()
-              dapui.close({})
-            end
-          end,
-        },
-        -- virtual text for the debugger
-        {
-          "theHamsta/nvim-dap-virtual-text",
-          opts = {},
-        },
-
-        -- which key integration
-        {
-          "folke/which-key.nvim",
-          optional = true,
-          opts = {
-            defaults = {
-              ["<leader>d"] = { name = "+debug" },
-            },
-          },
-        },
-
-        -- mason.nvim integration
-        {
-          "jay-babu/mason-nvim-dap.nvim",
-          dependencies = "mason.nvim",
-          cmd = { "DapInstall", "DapUninstall" },
-          opts = {
-            -- Makes a best effort to setup the various debuggers with
-            -- reasonable debug configurations
-            automatic_installation = true,
-
-            -- You can provide additional configuration to the handlers,
-            -- see mason-nvim-dap README for more information
-            handlers = {},
-
-            -- You'll need to check that you have the required things installed
-            -- online, please don't ask me how to install them :)
-            ensure_installed = {
-              -- Update this to ensure that you have the debuggers for the langs you want
-            },
-          },
-        },
-      },
-
-      -- stylua: ignore
-      keys = {
-        { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
-        { "<leader>db", function() require("dap").toggle_breakpoint() end,                                    desc = "Toggle Breakpoint" },
-        { "<leader>dc", function() require("dap").continue() end,                                             desc = "Continue" },
-        { "<leader>da", function() require("dap").continue({ before = get_args }) end,                        desc = "Run with Args" },
-        { "<leader>dC", function() require("dap").run_to_cursor() end,                                        desc = "Run to Cursor" },
-        { "<leader>dg", function() require("dap").goto_() end,                                                desc = "Go to Line (No Execute)" },
-        { "<leader>di", function() require("dap").step_into() end,                                            desc = "Step Into" },
-        { "<leader>dj", function() require("dap").down() end,                                                 desc = "Down" },
-        { "<leader>dk", function() require("dap").up() end,                                                   desc = "Up" },
-        { "<leader>dl", function() require("dap").run_last() end,                                             desc = "Run Last" },
-        { "<leader>do", function() require("dap").step_out() end,                                             desc = "Step Out" },
-        { "<leader>dO", function() require("dap").step_over() end,                                            desc = "Step Over" },
-        { "<leader>dp", function() require("dap").pause() end,                                                desc = "Pause" },
-        { "<leader>dr", function() require("dap").repl.toggle() end,                                          desc = "Toggle REPL" },
-        { "<leader>ds", function() require("dap").session() end,                                              desc = "Session" },
-        { "<leader>dt", function() require("dap").terminate() end,                                            desc = "Terminate" },
-        { "<leader>dw", function() require("dap.ui.widgets").hover() end,                                     desc = "Widgets" },
-      },
-
-      config = function()
-        local Config = require("lazyvim.config")
-        vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
-
-        for name, sign in pairs(Config.icons.dap) do
-          sign = type(sign) == "table" and sign or { sign }
-          vim.fn.sign_define(
-            "Dap" .. name,
-            { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
-          )
-        end
-      end,
-    }
-  }
-end
-
 local isDebugging = false
 local isFEDebugging = false
 
@@ -166,8 +57,8 @@ return {
     },
   },
   keys = {
-    { "<leader>C", toggleDebugging,   desc = "Debug - Backend Debug" },
-    { "<leader>c", toggleFEDebugging, desc = "Debug - Frontend Debug" },
+    { "<leader>c", toggleDebugging,   desc = "Debug - Backend Debug" },
+    { "<leader>C", toggleFEDebugging, desc = "Debug - Frontend Debug" },
     {
       "<leader>v",
       function()
@@ -214,7 +105,7 @@ return {
   config = function()
     require("dap-vscode-js").setup({
       debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
-      adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
+      adapters = { "node", "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
     })
 
     require("dap").adapters.firefox = {
@@ -254,6 +145,21 @@ return {
           resolveSourceMapLocations = nil,
           program = "${file}",
           cwd = "${workspaceFolder}",
+          skipFiles = { "**/node_modules/**", "<node_internals>/**", "http?(s):/**", "**/google.com/**" },
+        },
+        {
+          name = "Attach (node)",
+          type = "node",
+          request = "attach",
+          port = 9229,
+          cwd = "/",
+          resolveSourceMapLocations = nil,
+          attachExistingChildren = false,
+          autoAttachChildProcesses = false,
+          restart = true,
+          autoReload = {
+            enable = true,
+          },
           skipFiles = { "**/node_modules/**", "<node_internals>/**", "http?(s):/**", "**/google.com/**" },
         },
       }
