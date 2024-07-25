1776e8d7a81fe3f87046e0285c6d6585ed91c36a
teekm <tkimm@me.com>
2024-07-25 21:41:09 +0200

    Add markdownpreview shortcut

--- lua/plugins/markdownpreview.lua
+++ lua/plugins/markdownpreview.lua
@@ -3,6 +3,9 @@ return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     build = "cd app && yarn install",
+    keys = {
+      { "<leader>M", "<cmd>MarkdownPreviewToggle<CR>", desc = "View Markdown", { silent = true } },
+    },
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
     end,
