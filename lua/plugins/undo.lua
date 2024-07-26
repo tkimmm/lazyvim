52f450dceabcc6babd5fc95913677e5c20786ce8
teekm@mbp2023 <tkimm@me.com>
2024-07-26 14:51:31 +0200

    Add undotree

---
+++ lua/plugins/undo.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "mbbill/undotree",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>U", "<cmd>UndotreeToggle<CR>", desc = "Undo Tree", { silent = true } },
+    },
+  },
+}
