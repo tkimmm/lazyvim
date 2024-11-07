b1ade6e7f186f06e76594ad65bb459c0e69fdcf7
teekm@mbp2023 <tkimm@me.com>
2024-11-07 21:28:06 +0100

    Remove cursorline option

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazygit_config = false
+vim.o.cursorline = false
 -- This enables word wrapping
 vim.opt.wrap = true
 -- Disable diagnostic by default
